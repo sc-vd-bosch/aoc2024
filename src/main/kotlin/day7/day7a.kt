0fe36755dd2a11da4b5926ae39f81bdd1f417f65
sc-vd-bosch <s.c.bosch@hotmail.com>
2024-12-11 09:22:05 +0100

    day 7

--- src/main/kotlin/day7/day7a.kt
+++ src/main/kotlin/day7/day7a.kt
@@ -1,40 +1,60 @@
 package org.example.day7
 
 import java.io.File
-import kotlin.math.absoluteValue
+import java.io.IO.println
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val input = File("src/main/resources/day7a-example.txt").readLines()
+    val input = File("src/main/resources/day7a.txt").readLines()
 
     val bridge = mutableListOf<Calibration>()
 
     input.map { s -> s.split(":") }
         .forEach { s ->
-            val list = s[1].split(" ").filter{ s -> s.isNotEmpty() }.map { s -> s.toInt() }
-            bridge.add(Calibration(s[0].toInt(),list))
+            val list = s[1].split(" ").filter{ s -> s.isNotEmpty() }.map { s -> s.toLong() }
+//            println("${s[0].toLong()}: $list")
+            println(list.count())
+            bridge.add(Calibration(s[0].toLong(),list))
         }
 
+    var output = 0L
 
-    var output = 0
-
-    bridge.forEach { c -> c.print() }
+//    bridge.forEach { c -> c.print() }
     bridge.filter{ c -> c.valuePossible() }.forEach { c -> output += c.testValue }
 
     println(output)
 }
 
-class Calibration(val testValue: Int, val list: List<Int>) {
+class Calibration(val testValue: Long, val list: List<Long>) {
 
     fun valuePossible(): Boolean {
-        return when(list.count()) {
-            0 -> true
-            1 -> list[0] == testValue
-            2 -> list[0] + list[1] == testValue || list[0] * list[1] == testValue
-            else -> false
+        print()
+        return something(testValue, list, Operator.ADD)
+    }
+
+    private fun something(value: Long, list2: List<Long>, op: Operator): Boolean {
+        println("in something: $value: $list2,  $op")
+
+        if (list2.count() == 1) {
+            return list2[0] == value
         }
 
+        if (value < 0) {
+            return false
+        }
+
+        val lastValue = list2[list2.count()-1]
+
+        val newDividedValue = value / lastValue
+
+        val sublist = list2.subList(0, list2.count() - 1)
+        val isConcatinated = value.toString().endsWith(lastValue.toString()) && value.toString().length != lastValue.toString().length
+
+        println(isConcatinated)
+
+        return value ==  newDividedValue * lastValue && something(newDividedValue, sublist, Operator.MUL)
+                || something(value - lastValue, sublist, Operator.ADD)
+                || isConcatinated
+                    && something(value.toString().dropLast(lastValue.toString().count()).toLong(), sublist, Operator.CONCAT)
     }
 
     fun print() {
@@ -43,5 +63,5 @@ class Calibration(val testValue: Int, val list: List<Int>) {
 }
 
 enum class Operator {
-    ADD, MULTIPLY
+    ADD, MUL, CONCAT
 }
